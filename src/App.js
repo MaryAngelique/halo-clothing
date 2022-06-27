b9edeeb663d717a7796b5686faf12fe4252420e7
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-27 17:13:02 -0400

    Implemented navigation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home/home.componentt";
+import Home from "./navigation/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 
 const App = () => {
 
   return(
-    <Routes>
-      <Route path="/" index element={<Home />} />
-    </Routes>
+    <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+    </Route>
   );
 }
 

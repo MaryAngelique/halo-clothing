04352c4b3cc8e4415b0b60eb2bb01d9395522046
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-27 17:52:01 -0400

    Implemented navifation styling and routed to render the navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./navigation/home/home.component";
+import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 
 
 const App = () => {
+  const Shop = () => {
+      return <h1>I am the shop page</h1>;
+  };
 
   return(
-    <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-    </Route>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 

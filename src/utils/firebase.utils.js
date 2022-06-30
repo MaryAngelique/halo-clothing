21f0ad51cd21676d9c1a54234f487080e2729411
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-30 15:12:05 -0400

    Updated firebase utility

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -15,8 +15,10 @@ import {
     getFirestore, 
     doc,
     getDoc, 
-    setDoc 
-} from 'firebase/firestore';
+    setDoc,
+    collection,
+    writeBatch,
+} from "firebase/firestore";
 
   
 const firebaseConfig = {
@@ -42,10 +44,26 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+    ) => {
+      const batch = writeBatch(db);
+      const collectionRef = collection(db, collectionKey);
+      
+      objectsToAdd.forEach((object) => {
+          const docRef = doc(collectionRef, object.title.toLowerCase());
+          batch.set(docRef, object);
+      });
+
+      await batch.commit();
+      console.log("done");
+};
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation  = {}) => {
   if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -61,7 +79,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
         ...additionalInformation
       });
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log("error creating the user", error.message);
     }
   }
 

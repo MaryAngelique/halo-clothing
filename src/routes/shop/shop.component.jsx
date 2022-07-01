15e475ea892eae73dcd81b31c26798442aaaf47e
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-01 11:54:10 -0400

    Updated shop component route

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
@@ -10,11 +10,20 @@ const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext);
 
     return (
-        <div className="products-container">
-            {categoriesMap.map((product) => (
-                <ProductCard key={product.id} product={product} />
+        <Fragment>
+            {Object.keys(categoriesMap).map((title) => (
+                <Fragment key={title}>
+                <h2>{title}</h2>
+
+                <div className='products-container'>
+                    {categoriesMap[title].map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+                
+                </Fragment>
             ))}
-        </div>
+        </Fragment>
     );
 };
 

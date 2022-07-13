ae832dc5d66ec02d9b0238f5034036a1dc30ef76
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-13 17:34:06 -0400

    Updated App Js to render dynamic component as regular component

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -4,7 +4,6 @@ import { useDispatch } from "react-redux";
 
 import { fetchCategoriesStart } from "../../store/categories/categories.action";
 
-
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 import Category from "../category/category.component";
 

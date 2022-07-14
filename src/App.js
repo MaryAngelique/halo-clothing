3dce1643f21f4e07b85b5eed5f1e71cacbe12cb2
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-14 13:55:49 -0400

    Updated App Js to render global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect, lazy, Suspense } from "react";
-import { useDispatch } from "react-redux";
-
+import { useDispatch } from "react-redux"
+ 
 import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/user/user.action";
+import { GlobalStyle } from "./global.styles";
 
 const Navigation = lazy(() =>
   import("./routes/navigation/navigation.component")
@@ -24,14 +25,15 @@ const App = () => {
 
   return(
     <Suspense fallback={<Spinner />}>
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="shop/*" element={<Shop />} />
-                <Route path="auth" element={<Authentication />} />
-                <Route path="checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="shop/*" element={<Shop />} />
+            <Route path="auth" element={<Authentication />} />
+            <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
     </Suspense>
   );
 }

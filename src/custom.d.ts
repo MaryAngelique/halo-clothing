abf1a76e0dfd43b80201c93998a77efa484edc78
Angelique Abacajan <ma_utsey@yahoo.com>
2022-07-14 13:03:19 -0400

    Syntactical error in custom ts file fixed

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,6 @@
 declare module "*.svg" {
     import React = require("react");
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
+    export const RC: React.FC<React.SVGProps<SVGSVGElement>>;
+    const source: string;
     export default src;
 }
